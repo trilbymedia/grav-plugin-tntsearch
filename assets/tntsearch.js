64a9bceeb881697fc1781534898000bd9c9a0e1a
Andrew Scofield <apotropaics@gmail.com>
2017-11-02 16:13:07 -0700

    independent js for searching

--- assets/tntsearch.js
+++ assets/tntsearch.js
@@ -1,43 +1,82 @@
-jQuery(document).ready(function() {
-    var body = $('body');
-    body.on('keyup focus', '.tntsearch-field', function() {
-        var element = $(this),
-            container = element.closest('form').find('.tntsearch-results'),
-            value = element.val(),
-            data = element.data('tntsearch');
+function TntSearch(){
+  this.search_input = document.querySelector('.tntsearch-field');
+  this.container = document.querySelector('.tntsearch-results');
+  this.form = document.querySelector('.tntsearch-form');
 
-        if (!value.length) {
-            container.hide();
-            return false;
-        }
+  this.data = JSON.parse(this.search_input.dataset.tntsearch);
 
-        if (value.length < (data.min || 3)) { return false; }
+  this.snippet = this.data.snippet || '';
+  this.limit = this.data.limit || '';
+  this.uri = this.data.uri;
+  
+  this.timeout = '';
+  
+  if(this.search_input){
+    var _this = this;
+    this.search_input.addEventListener('keyup', function(){
+  
+    //delayed timer on this keyup event so it doesn't query the server a bunch of times unnecessarily 
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout( function(){ _this.search() }, 400);
+    })
+  }
+};
 
-        var snippet = data.snippet|| '',
-            limit = data.limit || '',
-            uri = data.uri;
 
-        $.ajax({
-            url: uri,
-            data: {
-                q: value,
-                sl: snippet,
-                l: limit,
-                ajax: true
-            },
-            success: function(result) {
-                container.show().html(result);
-            }
-        });
+TntSearch.prototype.search = function(){
+    var value = this.search_input.value;
+    
+    if (!value.length) {
+      this.container.style.display = 'none';
+      return false;
+    }
+    
+    //start searching after 3 letters
+  if (value.length < 3) { 
+      return false; 
+    }
+    
+    // stop form from submitting
+    this.form.addEventListener('submit', function(event){ 
+      event.preventDefault; 
+      return false 
     });
+    
+    // build query
+    var query = '?q=' + encodeURIComponent(value).replace(/%20/g,'+');
+    query += "&ajax=true&l="+this.limit+"&sl="+this.snippet
+    
+    var _this = this;
+    this.tntQuery(this.uri + query, function(data){
+    _this.container.style.display = 'block';
+    _this.container.innerHTML = data;
+  });
+  
+}
 
-    body.on('click', function(e) {
-        e.stopPropagation();
-        var dropdown = $('.tntsearch-dropdown'),
-            results = $('.tntsearch-results:visible:not(.tntsearch-inpage)');
 
-        if (dropdown.has(e.target).length === 0 && results.has(e.target).length === 0) {
-            results.hide();
-        }
-    });
-});
+TntSearch.prototype.tntQuery = function(url, success) {
+  var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+  xhr.open('GET', url);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+  };
+  
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.send();
+  return xhr;
+}
+
+  
+function docReady(){
+  var search = new TntSearch();
+}
+
+if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)){
+  docReady();
+} 
+else {
+  document.addEventListener('DOMContentLoaded', docReady);
+}
